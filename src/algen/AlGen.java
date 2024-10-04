6d2877d420d3ce6307b40702ce61e6c14cea86c3
Christian Diaz <chris.d.es@outlook.com>
2024-10-04 17:13:03 -0600

    agregar documentacion cambio de problema

--- src/algen/AlGen.java
+++ src/algen/AlGen.java
@@ -4,81 +4,110 @@
  */
 package algen;
 
-import org.jgap.Chromosome;
-import org.jgap.Configuration;
-import org.jgap.Gene;
-import org.jgap.Genotype;
-import org.jgap.IChromosome;
-import org.jgap.InvalidConfigurationException;
-import org.jgap.impl.DefaultConfiguration;
-import org.jgap.impl.IntegerGene;
+import org.jgap.Chromosome; // Importa la clase Chromosome de JGAP
+import org.jgap.Configuration; // Importa la clase Configuration de JGAP
+import org.jgap.Gene; // Importa la clase Gene de JGAP
+import org.jgap.Genotype; // Importa la clase Genotype de JGAP
+import org.jgap.IChromosome; // Importa la interfaz IChromosome de JGAP
+import org.jgap.InvalidConfigurationException; // Importa la excepción InvalidConfigurationException de JGAP
+import org.jgap.impl.DefaultConfiguration; // Importa la implementación DefaultConfiguration de JGAP
+import org.jgap.impl.IntegerGene; // Importa la clase IntegerGene de JGAP
+import org.jgap.impl.MutationOperator; // Importa la clase MutationOperator de JGAP
 
 /**
- *
- * @author Chris
+ * Clase principal que implementa un algoritmo genético.
  */
 public class AlGen {
 
+    /**
+     * Constructor de la clase AlGen.
+     * Inicializa la configuración del algoritmo genético y ejecuta la evolución.
+     * @throws org.jgap.InvalidConfigurationException
+     */
     public AlGen() throws InvalidConfigurationException {
-        //modificar poblacion y generaciones en caso de que los resultados del problema plantead no sean los adecuados
-        final int tamPoblacion = 1000;
-        final int nGeneraciones = 71420;
-        int valMaximo[] = {800, 800};
-        int valMinimo[] = new int[]{-1, -1};
+        // Definición del tamaño de la población y el número de generaciones
+        final int tamPoblacion = 10000; // Número de individuos en la población
+        final int nGeneraciones = 5000;  // Número de generaciones para evolucionar
+        // Definición de los valores máximos y mínimos para cada gen
+        int valMaximo[] = {800, 800, 800, 800}; // Valores máximos para los genes
+        int valMinimo[] = new int[]{-1, -1, -1, -1}; // Valores mínimos para los genes
+        
+        // Imprime encabezados y detalles del algoritmo
         System.out.println("===== Algoritmo Genetico =====");
-        System.out.println("Maximizar la funcion "
-                + "z = 1200x + 1400y");
-        System.out.print("\nnumero de individuos="
-                + tamPoblacion);
+        System.out.println("Maximizar la funcion z = 6x + 5y +3w +7z");
+        System.out.print("\nnumero de individuos=" + tamPoblacion);
+        // Imprime el rango de cada gen
         for (int i = 0; i < valMinimo.length; i++) {
-            System.out.printf("\nrango %d[%d , %d]", i,
-                    valMinimo[i], valMaximo[i]);
+            System.out.printf("\nrango %d[%d , %d]", i, valMinimo[i], valMaximo[i]);
         }
 
+        // Imprime encabezados para la salida de generaciones
         System.out.println("\n\nGeneracion  Aptitud[mejor]    MejorIndividuo");
         System.out.print("--------  ------------    -------------------");
 
-        Configuration conf = new DefaultConfiguration();
-        FuncionAptitud funcion = new FuncionAptitud();
+        // Configuración del algoritmo genético
+        Configuration conf = new DefaultConfiguration(); // Crea una nueva configuración
+        FuncionAptitud funcion = new FuncionAptitud(); // Crea una instancia de la función de aptitud
 
-        conf.setFitnessFunction(funcion);
-        conf.setPreservFittestIndividual(true);
-        
+        // Configuración de la tasa de mutación (5% de la población)
+        double mutationRate = 0.05;  // Tasa de mutación del 5%
+        int numberOfMutations = (int) (tamPoblacion * mutationRate); // Calcula el número de individuos a mutar
 
-        Gene[] genMuestra = new Gene[2];
+        // Inicializa el operador de mutación y establece la tasa de mutación
+        MutationOperator mutationOperator = new MutationOperator(conf);
+        mutationOperator.setMutationRate(numberOfMutations); // Establece el número de mutaciones en cada generación
+
+        // Configura la función de aptitud y preservación del mejor individuo
+        conf.setFitnessFunction(funcion); // Asigna la función de aptitud a la configuración
+        conf.setPreservFittestIndividual(true); // Activa la preservación del mejor individuo en la población
+
+        // Creación de un arreglo de genes
+        Gene[] genMuestra = new Gene[4]; // Crea un arreglo para 4 genes
+        // Inicializa cada gen con valores mínimos y máximos
         genMuestra[0] = new IntegerGene(conf, valMinimo[0], valMaximo[0]);
         genMuestra[1] = new IntegerGene(conf, valMinimo[1], valMaximo[1]);
+        genMuestra[2] = new IntegerGene(conf, valMinimo[2], valMaximo[2]);
+        genMuestra[3] = new IntegerGene(conf, valMinimo[3], valMaximo[3]);
 
+        // Crea un cromosoma a partir de la muestra de genes
         Chromosome cromosomaMuestra = new Chromosome(conf, genMuestra);
-        conf.setSampleChromosome(cromosomaMuestra);
-        conf.setPopulationSize(tamPoblacion);
+        conf.setSampleChromosome(cromosomaMuestra); // Asigna el cromosoma de muestra a la configuración
+        conf.setPopulationSize(tamPoblacion); // Establece el tamaño de la población
 
+        // Crea una población inicial aleatoria
         Genotype poblacion = Genotype.randomInitialGenotype(conf);
-        //Inicio de funcionalidad 
+
+        // Inicio del proceso evolutivo
         for (int generacion = 1; generacion < nGeneraciones; generacion++) {
-            poblacion.evolve();
+            poblacion.evolve(); // Evoluciona la población
 
-            // Obtener la mejor solución
-            IChromosome mejorIndividuo = poblacion.getFittestChromosome();
-            double mejorAptitud = mejorIndividuo.getFitnessValue();
-            System.out.printf("\n%3d  %4.3f  ", generacion, mejorAptitud);
+            // Obtiene la mejor solución de la población actual
+            IChromosome mejorIndividuo = poblacion.getFittestChromosome(); // Obtiene el mejor individuo
+            double mejorAptitud = mejorIndividuo.getFitnessValue(); // Obtiene la aptitud del mejor individuo
+            System.out.printf("\n%3d  %4.3f  ", generacion, mejorAptitud); // Imprime la generación y su aptitud
 
+            // Imprime los valores de los genes del mejor individuo
             for (Gene g : mejorIndividuo.getGenes()) {
-                System.out.printf("%d ", (int) g.getAllele());
+                System.out.printf("%d ", (int) g.getAllele()); // Imprime el valor del gen
             }
 
-            /*for (Gene g : mejorIndividuo.getGenes()) {
+            /* 
+            Descomentar la siguiente sección para imprimir la representación persistente del mejor individuo
+            for (Gene g : mejorIndividuo.getGenes()) {
                 System.out.printf("%s ", g.getPersistentRepresentation());
-            }*/
+            }
+            */
         }
 
     }
 
-    public static void main(String[] args)
-            // TODO code application logic here
-
-            throws InvalidConfigurationException {
-        new AlGen();
+    /**
+     * Método principal para ejecutar el algoritmo genético.
+     * @param args
+     * @throws org.jgap.InvalidConfigurationException
+     */
+    public static void main(String[] args) throws InvalidConfigurationException {
+        new AlGen(); // Crea una instancia de AlGen y ejecuta el algoritmo
     }
 
 }
--- src/algen/FuncionAptitud.java
+++ src/algen/FuncionAptitud.java
@@ -2,35 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package algen;
-import org.jgap.IChromosome;
-import org.jgap.FitnessFunction;
+package algen; // Paquete donde se encuentra la clase
+
+import org.jgap.IChromosome; // Importa la interfaz IChromosome de JGAP
+import org.jgap.FitnessFunction; // Importa la clase FitnessFunction de JGAP
 
 /**
- *
- * @author Chris
+ * Clase que representa la función de aptitud para el algoritmo genético.
+ * Esta clase extiende la clase FitnessFunction de JGAP.
  */
 public class FuncionAptitud extends FitnessFunction {
-    
+
+    /**
+     * Método que evalúa la aptitud de un cromosoma.
+     * 
+     * @param e El cromosoma a evaluar.
+     * @return La aptitud del cromosoma, que es el valor de la función objetivo si cumple las restricciones, o 0 en caso contrario.
+     */
     @Override 
-    protected double evaluate(IChromosome e){
-    int x0 = (int) e.getGene(0).getAllele();
-int x1 = (int) e.getGene(1).getAllele();
-double z = 1200 * x0 + 1400 * x1;
+    protected double evaluate(IChromosome e) {
+        // Obtiene los valores de los genes del cromosoma
+        int x0 = (int) e.getGene(0).getAllele(); // Primer gene
+        int x1 = (int) e.getGene(1).getAllele(); // Segundo gene
+        int x2 = (int) e.getGene(2).getAllele(); // Tercer gene
+        int x3 = (int) e.getGene(3).getAllele(); // Cuarto gene
+        
+        // Calcula el valor de la función objetivo
+        double z = 6 * x0 + 5 * x1 + 3 * x2 + 7 * x3;
 
-// Restricciones
-boolean r1 = (x0 + 2 * x1 <= 800);
-boolean r2 = (2 * x0 + x1 <= 800);
-boolean r3 = (x0 + x1 <= 500);
-boolean r4 = (x0 >= 0 && x1 >= 0);
+        // Evaluación de restricciones
+        boolean r1 = (x0 + x1 + 3 * x3 <= 50); // Restricción 1
+        boolean r2 = (2 * x0 + x1 + 2 * x2 + x3 <= 150); // Restricción 2
+        boolean r3 = (x0 + x1 + x2 <= 80); // Restricción 3
+        boolean r4 = (x0 >= 0 && x1 >= 0 && x2 >= 0 && x3 >= 0); // Restricción 4 (no negativos)
 
-if (r1 && r2 && r3 && r4) {
-    // Si se cumplen las restricciones
-    // entonces la solución es candidata
-    return z;
-} else {
-    // No cumplió con las restricciones
-    return 0;
+        // Verifica si se cumplen todas las restricciones
+        if (r1 && r2 && r3 && r4) {
+            // Si se cumplen las restricciones, retorna el valor de la función objetivo
+            return z;
+        } else {
+            // Si no se cumplen las restricciones, retorna 0 (indica una solución no válida)
+            return 0;
+        }
+    }
 }
-    
-    }}
